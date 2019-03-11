5979b1e60b2f572d34bde91316d89952ef506345
Sidney Andrews <sidney@seesharprun.net>
2019-03-10 23:45:06 -0400

    Update ProcessDocuments.cs

--- 03/Starter/Contoso.Events.Worker/ProcessDocuments.cs
+++ 03/Starter/Contoso.Events.Worker/ProcessDocuments.cs
@@ -1,11 +1,14 @@
 using Contoso.Events.Data;
 using Contoso.Events.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Contoso.Events.Worker
@@ -13,39 +16,34 @@ namespace Contoso.Events.Worker
     public static class ProcessDocuments
     {
         private static ConnectionManager _connection = new ConnectionManager();
-        private static RegistrationContext _registrationsContext = _connection.GetCosmosContext();
+
 
         [FunctionName("ProcessDocuments")]
-        public static async Task Run(Stream input, string name, Stream output, TraceWriter log)
+        public static async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")]HttpRequest request, TraceWriter log)
         {
-            log.Info($"Request received to generate sign-in sheet for event: {name}");
+            string message = request.Query["eventkey"];
 
-            
+            log.Info($"Request received to generate sign-in sheet for event: {message}");
 
+            var registrants = await ProcessHttpRequestMessage(message);
 
-            log.Info($"Request received to generate sign-in sheet for event: {name}");
+            log.Info($"Registrants: {String.Join(", ", registrants)}");
+
+            log.Info($"Request completed for event: {message}");
         }
 
-        private static async Task<MemoryStream> ProcessStorageMessage(string eventKey)
+        private static async Task<List<string>> ProcessHttpRequestMessage(string eventKey)
         {
-            SignInDocumentGenerator documentGenerator = new SignInDocumentGenerator();
-
             using (EventsContext eventsContext = _connection.GetSqlContext())
             {
                 await eventsContext.Database.EnsureCreatedAsync();
-                await _registrationsContext.ConfigureConnectionAsync();
+
 
                 Event eventEntry = await eventsContext.Events.SingleOrDefaultAsync(e => e.EventKey == eventKey);
 
-                List<string> registrants = await _registrationsContext.GetRegistrantsForEvent(eventKey);
+                List<string> registrants = new List<string>();
 
-                MemoryStream stream = documentGenerator.CreateSignInDocument(eventEntry.Title, registrants);
-                stream.Seek(0, SeekOrigin.Begin);
-
-                eventEntry.SignInDocumentUrl = $"{eventKey}.docx";
-                await eventsContext.SaveChangesAsync();
-
-                return stream;
+                return registrants;
             }
         }
     }
