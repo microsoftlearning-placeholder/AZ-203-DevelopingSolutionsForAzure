372cfc60d2d65bfc896e48b767944dcd7e8f6ab6
Sidney Andrews <sidney@seesharprun.net>
2019-03-10 23:46:47 -0400

    Update RegistrationContext.cs

--- 03/Starter/Contoso.Events.Data/RegistrationContext.cs
+++ 03/Starter/Contoso.Events.Data/RegistrationContext.cs
@@ -1,4 +1,4 @@
-﻿using Contoso.Events.Models;
+using Contoso.Events.Models;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Options;
@@ -12,58 +12,31 @@ namespace Contoso.Events.Data
 {
     public class RegistrationContext
     {
-        protected Database Database { get; set; }
-
-        protected DocumentCollection Collection { get; set; }
-
-        protected DocumentClient Client { get; set; }
-
         protected CosmosSettings CosmosSettings { get; set; }
 
         public RegistrationContext(IOptions<CosmosSettings> cosmosSettings)
         {
             CosmosSettings = cosmosSettings.Value;
-            Client = new DocumentClient(new Uri(CosmosSettings.EndpointUrl), CosmosSettings.AuthorizationKey);
         }
 
         public async Task ConfigureConnectionAsync()
         {
-            Database = await Client.CreateDatabaseIfNotExistsAsync(new Database { Id = CosmosSettings.DatabaseId });
-            Collection = await Client.CreateDocumentCollectionIfNotExistsAsync(Database.SelfLink, new DocumentCollection { Id = CosmosSettings.CollectionId });
+            await Task.FromResult(default(object));
         }
 
         public async Task<int> GetRegistrantCountAsync()
         {
-            FeedOptions options = new FeedOptions { EnableCrossPartitionQuery = true };
-            IDocumentQuery<int> query = Client.CreateDocumentQuery<int>(Collection.SelfLink, "SELECT VALUE COUNT(1) FROM registrants", options).AsDocumentQuery();
-
-            int count = 0;
-            while (query.HasMoreResults)
-            {
-                FeedResponse<int> results = await query.ExecuteNextAsync<int>();
-                count += results.Sum();
-            }
-            return count;
+            return await Task.FromResult(default(int));
         }
 
         public async Task<List<string>> GetRegistrantsForEvent(string eventKey)
         {
-            IDocumentQuery<GeneralRegistration> query = Client.CreateDocumentQuery<GeneralRegistration>(Collection.SelfLink).Where(r => r.EventKey == eventKey).AsDocumentQuery();
-
-            List<string> registrants = new List<string>();
-            while (query.HasMoreResults)
-            {
-                FeedResponse<GeneralRegistration> results = await query.ExecuteNextAsync<GeneralRegistration>();
-                IEnumerable<string> resultNames = results.Select(r => $"{r.FirstName} {r.LastName}");
-                registrants.AddRange(resultNames);
-            }
-            return registrants;
+            return await Task.FromResult(default(List<string>));
         }
 
         public async Task<string> UploadEventRegistrationAsync(dynamic registration)
         {
-            ResourceResponse<Document> response = await Client.CreateDocumentAsync(Collection.SelfLink, registration);
-            return response.Resource.Id;
+            return await Task.FromResult(default(string));
         }
     }
 }
